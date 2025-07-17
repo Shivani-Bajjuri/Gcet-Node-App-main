7e34415fb42424f91f3d9d8b6d8c704fd511d678
Shivani-Bajjuri <bajjurishivani30@gmail.com>
2025-07-17 16:47:54 +0530

    .

--- models/productModel.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose'
-const productSchema = mongoose.Schema({
-  img: { type: String },
-  name: { type: String },
-  price: { type: Number }
-});
-
-export default mongoose.model("Product", productSchema);
